f0a8d70ce1d883c7b4689997020582aab05483de
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-04-28 11:12:37 -0500

    added jsonserialization

---
+++ BMUtilityPack/Utilities/NSJSONSerialization+Utilities/NSJSONSerialization+Utilities.h
@@ -0,0 +1,13 @@
+//
+//  NSJSONSerialization+Utilities.h
+//  Dryv-DryCleaners
+//
+//  Created by Jeffrey Camealy on 4/27/13.
+//  Copyright (c) 2013 Ora Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSJSONSerialization (Utilities)
+
+@end
---
+++ BMUtilityPack/Utilities/NSJSONSerialization+Utilities/NSJSONSerialization+Utilities.m
@@ -0,0 +1,13 @@
+//
+//  NSJSONSerialization+Utilities.m
+//  Dryv-DryCleaners
+//
+//  Created by Jeffrey Camealy on 4/27/13.
+//  Copyright (c) 2013 Ora Interactive. All rights reserved.
+//
+
+#import "NSJSONSerialization+Utilities.h"
+
+@implementation NSJSONSerialization (Utilities)
+
+@end
