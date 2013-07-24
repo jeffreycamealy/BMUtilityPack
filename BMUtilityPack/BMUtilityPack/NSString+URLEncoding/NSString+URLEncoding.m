91dd26aeea488b35afe03372018716dc95f1b314
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-07-24 15:38:38 -0500

    upgrade

--- BMUtilityPack/BMUtilityPack/NSString+URLEncoding/NSString+URLEncoding.m
+++ BMUtilityPack/BMUtilityPack/NSString+URLEncoding/NSString+URLEncoding.m
@@ -5,7 +5,22 @@
 @implementation NSString (URLEncoding)
 
 - (NSDictionary *)dictionaryRepresentation {
-    return nil;
+    NSString *plusesToSpaces = [self stringByReplacingOccurrencesOfString:@"+" withString:@" "];
+    NSString *percentEscapesRemoved = [plusesToSpaces stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    NSArray *keyValuePairStrings = [percentEscapesRemoved componentsSeparatedByString:@"&"];
+    
+    NSMutableDictionary *dict = NSMutableDictionary.new;
+    for (NSString *keyValuePairString in keyValuePairStrings) {
+        NSMutableArray *keyValuePair = [keyValuePairString componentsSeparatedByString:@"="].mutableCopy;
+        id value = keyValuePair.lastObject;
+        [keyValuePair removeLastObject];
+        id key = keyValuePair.lastObject;
+        
+        if (value && key) [dict setValue:value forKey:key];
+    }
+    
+    return dict.copy;
 }
 
 @end
